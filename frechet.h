437f96a8d4b7b5107392ee9c9d3af00ce6a35b8b
Acey <huiwang.e@gmail.com>
2016-03-20 23:20:44 +0800

    Update frechet.h

--- frechet.h
+++ frechet.h
@@ -1,4 +1,15 @@
+#include<math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <float.h>
+
 typedef struct {
     double x;
     double y;
-}point;
+}__point;
+
+void print(double **ca, int m, int n);
+double euc_dist(__point  pt1, __point  pt2);
+double min(double a, double b, double c);
+double _c(double **ca, int i, int j, __point *p, __point *q);
+double distance(int m, int n, __point *p, __point *q);
